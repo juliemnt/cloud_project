922fb2437d42a86c8420a94850ca4c3494fcacbb
juliemnt <julie.monnot@efrei.net>
2023-12-07 13:30:37 +0100

    initial commit

---
+++ src/test/java/fr/efrei/ApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.efrei;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
